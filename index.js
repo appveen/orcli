43c079d69c7c0b89ba56f3e99284583a4adc69e4
Jugnu <jugnu@appveen.com>
2019-06-26 17:50:51 +0530

    1st build

--- index.js
+++ index.js
@@ -3,28 +3,32 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 const shell = require('shelljs');
+const makeDir = require('make-dir');
 const inquirer = require('inquirer');
 
 if (!process.env.JAVA_HOME) {
     console.log(chalk.cyan('***********************************'));
     console.log(chalk.red('JAVA_HOME not found, please set JAVA_HOME'));
     console.log(chalk.cyan('***********************************'));
+    process.exit();
 }
 
 if (!process.env.MAVEN_HOME) {
     console.log(chalk.cyan('***********************************'));
     console.log(chalk.red('MAVEN_HOME not found, please set MAVEN_HOME'));
     console.log(chalk.cyan('***********************************'));
+    process.exit();
 }
 
 if (!process.env.M2_HOME) {
     console.log(chalk.cyan('***********************************'));
     console.log(chalk.red('M2_HOME not found, please set M2_HOME'));
     console.log(chalk.cyan('***********************************'));
+    process.exit();
 }
 
 /**
- * @type {[{name:string,url:string,node:boolean}]}
+ * @type {[{name:string,url:string,node:boolean,short:string}]}
  */
 const repoList = JSON.parse(fs.readFileSync('repo-list.json', 'utf8'));
 
@@ -102,21 +106,36 @@ inquirer
     .then(answers => {
         console.log(answers);
         const workspace = path.join(__dirname, path.relative(__dirname, answers.workspace));
-        if (!fs.existsSync(workspace)) {
-            fs.mkdirSync(workspace);
-        }
+        // if (!fs.existsSync(workspace)) {
+            
+        // }
+        makeDir.sync(workspace);
         shell.cd(workspace);
         shell.rm('-rf', 'ODP_RELEASE');
         shell.rm('-rf', 'BRANCH');
+        let ODP_RELEASE;
         if (answers.releaseType == 'New Release') {
+            ODP_RELEASE = answers.release;
             shell.exec(`echo ${answers.release} > ODP_RELEASE`);
             shell.exec(`echo dev > BRANCH`);
         } else {
+            ODP_RELEASE = answers.patch;
             shell.exec(`echo ${answers.patch} > ODP_RELEASE`);
             shell.exec(`echo ${answers.branch} > BRANCH`);
         }
+        shell.pwd()
         for (let repo of repoList) {
+            shell.cd(workspace);
+            console.log(chalk.cyan('***********************************'));
+            console.log(chalk.green(`PROCESS STARTED FOR :: ${repo.name}`));
+            console.log(chalk.cyan('***********************************'));
+            if(repo.short){
+                shell.touch(`CLEAN_BUILD_${repo.short}`)
+            }
             if (fs.existsSync(repo.name)) {
+                shell.cd(repo.name);
+                shell.env['WORKSPACE'] = shell.pwd();
+                shell.exec(`git reset --hard`);
                 if (answers.releaseType !== 'New Release') {
                     shell.exec(`git checkout ${answers.branch}`);
                 } else {
@@ -125,19 +144,18 @@ inquirer
                 shell.exec(`git pull ${repo.url}`);
             } else {
                 shell.exec(`git clone ${repo.url}`);
+                shell.cd(repo.name);
                 if (answers.releaseType !== 'New Release') {
                     shell.exec(`git checkout ${answers.branch}`);
                 } else {
                     shell.exec(`git checkout dev`);
                 }
             }
-            shell.exec(`cd ${repo.name}`);
             if (repo.node) {
                 shell.exec(`npm i`);
             }
-            shell.exec(`export WORKSPACE=pwd`);
             if (fs.existsSync('scripts/build_image.sh')) {
-                shell.exec(`sh scripts/build_image.sh`);
+                shell.exec(`sh scripts/build_image.sh ${ODP_RELEASE}`);
             } else {
                 if (fs.existsSync('scripts/build_jar.sh')) {
                     shell.exec(`sh scripts/build_jar.sh`);
@@ -149,5 +167,8 @@ inquirer
                     shell.exec(`sh scripts/build_executables.sh`);
                 }
             }
+            console.log(chalk.cyan('***********************************'));
+            console.log(chalk.green(`PROCESS ENDED FOR :: ${repo.name}`));
+            console.log(chalk.cyan('***********************************'));
         }
     });
