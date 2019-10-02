229da11d5f16ac49a37b51ef891de21b7330abe6
Jugnu <jugnu@appveen.com>
2019-10-02 13:25:16 +0530

    release script update

--- release.js
+++ release.js
@@ -17,78 +17,102 @@ function trigger(answers) {
     shell.exec(`echo dev > BRANCH`);
     shell.pwd()
     for (let repo of repoList) {
+        if (repo.dependency && repo.dependency.length > 0) {
+            for (let i = 0; i < repo.dependency.length; i++) {
+                shell.cd(answers.workspace);
+                const dep = repo.dependency[i];
+                console.log(chalk.cyan('***********************************'));
+                console.log(chalk.green(`BUILD STARTED FOR DEPENDENCY :: ${dep}`));
+                console.log(chalk.cyan('***********************************'));
+                const tempRepo = repoList.find(e => e.name === dep);
+                buildImage(tempRepo, answers);
+                console.log(chalk.cyan('***********************************'));
+                console.log(chalk.green(`BUILD ENDED FOR DEPENDENCY :: ${dep}`));
+                console.log(chalk.cyan('***********************************'));
+            }
+        }
         shell.cd(answers.workspace);
         console.log(chalk.cyan('***********************************'));
         console.log(chalk.green(`PROCESS STARTED FOR :: ${repo.name}`));
         console.log(chalk.cyan('***********************************'));
-        if (repo.short && repo.short !== 'B2B') {
-            shell.touch(`CLEAN_BUILD_${repo.short}`)
-        }
-        if (fs.existsSync(repo.name)) {
-            let lastPull;
-            if (fs.existsSync(`LAST_PULL_${repo.name.toUpperCase()}`)) {
-                lastPull = shell.cat(`LAST_PULL_${repo.name.toUpperCase()}`);
-            }
-            shell.cd(repo.name);
-            shell.env['WORKSPACE'] = shell.pwd();
-            shell.exec(`git stash`);
-            shell.exec(`git checkout dev`);
-            shell.exec(`git pull`);
-            if (lastPull) {
-                console.log(chalk.cyan(''));
-                console.log(chalk.cyan('***********************************'));
-                console.log(chalk.green(`Changes found`));
-                console.log(chalk.cyan('***********************************'));
-                shell.exec(`git log --pretty=oneline --since="${lastPull}"`);
-                console.log(chalk.cyan('***********************************'));
-                console.log(chalk.cyan(''));
-            }
-            shell.exec(`echo ${new Date().toISOString()} > ../LAST_PULL_${repo.name.toUpperCase()}`);
-        } else {
-            shell.exec(`git clone ${repo.url}`);
-            shell.cd(repo.name);
-            shell.env['WORKSPACE'] = shell.pwd();
-            shell.exec(`git checkout dev`);
-            shell.exec(`echo ${new Date().toISOString()} > ../LAST_PULL_${repo.name.toUpperCase()}`);
-        }
-        if (repo.node) {
-            shell.exec(`npm i`);
-        }
-        if (fs.existsSync('scripts/build_image.sh')) {
-            shell.exec(`sh scripts/build_image.sh ${ODP_RELEASE}`);
-            shell.cd(answers.saveLocation);
-            const imageName = `odp:${repo.short.toLowerCase()}.${ODP_RELEASE}`;
-            const tarName = `odp_${repo.short.toLowerCase()}.${ODP_RELEASE}.tar`;
-            if (fs.existsSync(`${tarName}`)) {
-                shell.rm('-rf', `${tarName}`);
-            }
-            if (fs.existsSync(`${tarName}.bz2`)) {
-                shell.rm('-rf', `${tarName}.bz2`);
-            }
-            shell.exec(`docker save -o ${tarName} ${imageName}`)
-                .exec(`bzip2 odp_${repo.short.toLowerCase()}.${ODP_RELEASE}.tar`);
-            if (repo.short === 'SM') {
-                shell.exec(`docker save -o odp_base.${ODP_RELEASE}.tar odp:base.${ODP_RELEASE}`)
-                    .exec(`bzip2 odp_base.${ODP_RELEASE}.tar`);
-            }
-            if (repo.short === 'B2B') {
-                shell.exec(`docker save -o odp_b2b.runner.${ODP_RELEASE}.tar odp:b2b.runner.${ODP_RELEASE}`)
-                    .exec(`bzip2 odp_b2b.runner.${ODP_RELEASE}.tar`);
-            }
-        } else {
-            if (fs.existsSync('scripts/build_jar.sh')) {
-                shell.exec(`sh scripts/build_jar.sh`);
-            }
-            if (fs.existsSync('scripts/setup.sh')) {
-                shell.exec(`sh scripts/setup.sh`);
-            }
-            if (fs.existsSync('scripts/build_executables.sh')) {
-                shell.exec(`sh scripts/build_executables.sh`);
-            }
-        }
+        buildImage(repo, answers);
         console.log(chalk.cyan('***********************************'));
         console.log(chalk.green(`PROCESS ENDED FOR :: ${repo.name}`));
         console.log(chalk.cyan('***********************************'));
+    }
+}
+
+/**
+ * 
+ * @param {{name:string,url:string,node:boolean,short:string,dependency:string[]}} repo 
+ * @param {*} answers 
+ */
+function buildImage(repo, answers) {
+    const ODP_RELEASE = answers.patch || answers.branch;
+    if (repo.short && repo.short !== 'B2B') {
+        shell.touch(`CLEAN_BUILD_${repo.short}`)
+    }
+    if (fs.existsSync(repo.name)) {
+        let lastPull;
+        if (fs.existsSync(`LAST_PULL_${repo.name.toUpperCase()}`)) {
+            lastPull = shell.cat(`LAST_PULL_${repo.name.toUpperCase()}`);
+        }
+        shell.cd(repo.name);
+        shell.env['WORKSPACE'] = shell.pwd();
+        shell.exec(`git stash`);
+        shell.exec(`git checkout dev`);
+        shell.exec(`git pull`);
+        if (lastPull) {
+            console.log(chalk.cyan(''));
+            console.log(chalk.cyan('***********************************'));
+            console.log(chalk.green(`Changes found`));
+            console.log(chalk.cyan('***********************************'));
+            shell.exec(`git log --pretty=oneline --since="${lastPull}"`);
+            console.log(chalk.cyan('***********************************'));
+            console.log(chalk.cyan(''));
+        }
+        shell.exec(`echo ${new Date().toISOString()} > ../LAST_PULL_${repo.name.toUpperCase()}`);
+    } else {
+        shell.exec(`git clone ${repo.url}`);
+        shell.cd(repo.name);
+        shell.env['WORKSPACE'] = shell.pwd();
+        shell.exec(`git checkout dev`);
+        shell.exec(`echo ${new Date().toISOString()} > ../LAST_PULL_${repo.name.toUpperCase()}`);
+    }
+    if (repo.node) {
+        shell.exec(`npm i`);
+    }
+    if (fs.existsSync('scripts/build_image.sh')) {
+        shell.exec(`sh scripts/build_image.sh ${ODP_RELEASE}`);
+        shell.cd(answers.saveLocation);
+        const imageName = `odp:${repo.short.toLowerCase()}.${ODP_RELEASE}`;
+        const tarName = `odp_${repo.short.toLowerCase()}.${ODP_RELEASE}.tar`;
+        if (fs.existsSync(`${tarName}`)) {
+            shell.rm('-rf', `${tarName}`);
+        }
+        if (fs.existsSync(`${tarName}.bz2`)) {
+            shell.rm('-rf', `${tarName}.bz2`);
+        }
+        shell.exec(`docker save -o ${tarName} ${imageName}`)
+            .exec(`bzip2 odp_${repo.short.toLowerCase()}.${ODP_RELEASE}.tar`);
+        if (repo.short === 'SM') {
+            shell.exec(`docker save -o odp_base.${ODP_RELEASE}.tar odp:base.${ODP_RELEASE}`)
+                .exec(`bzip2 odp_base.${ODP_RELEASE}.tar`);
+        }
+        if (repo.short === 'B2B') {
+            shell.exec(`docker save -o odp_b2b.runner.${ODP_RELEASE}.tar odp:b2b.runner.${ODP_RELEASE}`)
+                .exec(`bzip2 odp_b2b.runner.${ODP_RELEASE}.tar`);
+        }
+    } else {
+        if (fs.existsSync('scripts/build_jar.sh')) {
+            shell.exec(`sh scripts/build_jar.sh`);
+        }
+        if (fs.existsSync('scripts/setup.sh')) {
+            shell.exec(`sh scripts/setup.sh`);
+        }
+        if (fs.existsSync('scripts/build_executables.sh')) {
+            shell.exec(`sh scripts/build_executables.sh`);
+        }
     }
 }
 
